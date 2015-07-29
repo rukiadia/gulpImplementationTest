ce0dae015814e4ab90125ad1caf0dec43a01adc7
Sho Yamada <yamadaPC@ymd.local>
2015-07-29 16:45:34 +0900

    run-sequenceでタスクの順番を制御

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,30 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
 	return gulp.src('assets/sass/*.sass')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('assets/css/'))
+		.pipe(gulp.dest('assets/css/'));
+});
+
+gulp.task('optimize', function(){
+	return gulp.src('assets/css/*.css')
 		.pipe(concat('all.css'))
+		.pipe(gulp.dest('assets/css/'))
 		.pipe(minify())
 		.pipe(rename({extname: '.min.css'}))
 		.pipe(gulp.dest('assets/css/minify/'))
+});
+
+// run-sequenceでタスクの実行順序を制御
+gulp.task('sass-optimize', function(){
+	runSequence(
+		'sass',
+		'optimize'
+	);
 });
 
 gulp.task('server', function(){
